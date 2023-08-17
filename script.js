6edd1d94225b1571cd5b273e86a0b59b8e4423ab
PARAS <mehtaparas1901@gmail.com>
2023-08-17 21:42:43 +0530

    Added connect cursor

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 
 // This function is used to make Locomotive and gsap work together
-function init(){
-    
-gsap.registerPlugin(ScrollTrigger);
+function init() {
 
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".main"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
+  gsap.registerPlugin(ScrollTrigger);
 
-// tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
-});
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".main"),
+    smooth: true
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+  // tell ScrollTrigger to use these proxy methods for the ".main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".main", {
+    scrollTop(value) {
+      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".main").style.transform ? "transform" : "fixed"
+  });
 
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
 
 }
 
@@ -66,46 +66,66 @@ var load = gsap.timeline()
 // })
 
 
-// for page 1 animation
-var tl = gsap.timeline({
-    scrollTrigger:{
-        trigger: ".page1 h1",
-        scroller:".main",
-        markers:true,
-        start:"top 30%",
-        end:"top 0%",
-        scrub:1
-
-    }
+// Cursor JS
+var crsr = document.querySelector("#cursor")
+var main = document.querySelector(".main")
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + 15 + "px"                       //dets ki vajah se x axis m follow krega cursor ko            
+  crsr.style.top = dets.y + 15 + "px"                        //dets ki vajah se y axis m follow krega cursor ko  
 })
 
-tl.to(".page1 h1" , {
-    x:-70,
-},"anime")
 
-tl.to(".page1 h2" , {
-    x:45,
-},"anime")
 
-tl.to(".page1 video",{
-    width:"90%"
-},"anime")
+// Page 1 animation
+
+// this animation is used to animate when page load
+gsap.from(".page1 h1,.page1 h2", {
+  y: 10,
+  rotate: 10,
+  opacity: 0,
+  delay: 0.5,
+  duration: 0.7
+})
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: ".main",
+    markers: true,
+    start: "top 30%",
+    end: "top 0%",
+    scrub: 1
+
+  }
+})
+
+tl.to(".page1 h1", {
+  x: -70,
+}, "anime")
+
+tl.to(".page1 h2", {
+  x: 45,
+}, "anime")
+
+tl.to(".page1 video", {
+  width: "90%"
+}, "anime")
 
 
 
 // to change bg color to white for page 2
 var tl2 = gsap.timeline({
-  scrollTrigger:{
-      trigger: ".page1 h1",
-      scroller:".main",
-      markers:true,
-      start:"top -120%",
-      end:"top -30%",
-      scrub:1
+  scrollTrigger: {
+    trigger: ".page1 h1",
+    scroller: ".main",
+    markers: true,
+    start: "top -120%",
+    end: "top -30%",
+    scrub: 1
 
   }
 })
 
-tl2.to(".main" , {
+tl2.to(".main", {
   backgroundColor: "#fff"
 })
